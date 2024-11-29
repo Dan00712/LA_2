28eb5a8255cb786c19c3b02a83cfca3ad40b4b1f
Daniel Steinmaurer <dan.steinmaurer@gmail.com>
2024-11-29 20:49:09 +0100

    add fits for doubleslit and double generator

--- ak/plot_all.sh
+++ ak/plot_all.sh
@@ -8,6 +8,7 @@ DST="./imgs/"
 #   plot 2.1 Interference
 ##################################################################################
 
+echo "plotting single slit"
 python plot_csv.py \
     --reg-mode=none \
     --outfile $DST/2.1_Interferenz.png \
@@ -25,21 +26,23 @@ python plot_csv.py \
 #   plot 2.3 Double Slit 
 ##################################################################################
 
+echo "plotting double slit"
 python plot_csv.py \
     --reg-mode=none \
     --outfile $DST/2.2_Doppelspalt.png \
     ./data/2.2_doppelspalt.csv
-# fit does is not stable
-: 'python plot_csv.py \
-    --reg-mode=double \
+# fit over double is not stable
+python plot_csv.py \
+    --reg-mode=single_max \
     --mirror \
     --outfile $DST/2.2_Doppelspalt.fit.png \
-    ./data/2.2_doppelspalt.csv'
+    ./data/2.2_doppelspalt.csv
 
 ##################################################################################
 #   plot 2.3 lloyd mirror
 ##################################################################################
 
+echo "plotting lloyd mirror"
 python plot_csv.py \
     --reg-mode=none \
     --outfile $DST/2.3_lloydspiegel.png \
@@ -49,15 +52,16 @@ python plot_csv.py \
 #   plot 2.4 double generator
 ##################################################################################
 
+echo "plotting double generator"
 python plot_csv.py \
     --reg-mode=none \
     --outfile $DST/2.4_doppelgenerator.png \
     ./data/2.4_doppelgenerator.csv
 
-: 'python plot_csv.py \
-    --reg-mode=double \
+python plot_csv.py \
+    --reg-mode=single_max \
     --mirror \
     --outfile $DST/2.4_doppelgenerator.fit.png \
-    ./data/2.4_doppelgenerator.csv'
+    ./data/2.4_doppelgenerator.csv
 
 deactivate
