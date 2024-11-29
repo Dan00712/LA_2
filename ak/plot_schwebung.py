91dbf90778fef556b5a3b2defe3ab66e31b2cf81
Daniel Steinmaurer <dan.steinmaurer@gmail.com>
2024-11-29 15:47:54 +0100

    plot beat of two signals with 40Hz and 41.5Hz

---
+++ ak/plot_schwebung.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+f1 = 40
+f2 = f1 + 1.5
+
+def f(t):
+    return np.sin(2*np.pi * f1 * t) + np.sin(2*np.pi * f2 * t)
+
+t = np.linspace(0, 1.25, 2000)
+a = f(t)
+a = a/ a.max()
+
+plt.plot(t, a)
+
+plt.savefig('imgs/beat.png')
