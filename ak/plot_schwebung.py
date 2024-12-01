43bd915462780c207918f73d688589df9e1e8c33
Daniel Steinmaurer <dan.steinmaurer@gmail.com>
2024-12-01 20:15:28 +0100

    improve beat plot

--- ak/plot_schwebung.py
+++ ak/plot_schwebung.py
@@ -2,18 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-f1 = 40
-f2 = f1 + 1.5
+f1 = 40 * 10**3
+f2 = f1 + 1.5*10**3
 
 
 def f(t):
     return np.sin(2 * np.pi * f1 * t) + np.sin(2 * np.pi * f2 * t)
 
 
-t = np.linspace(0, 1.25, 2000)
+t = np.linspace(0, 2*10**(-3), 5000)
 a = f(t)
 a = a / a.max()
 
-plt.plot(t, a)
+plt.plot(t*1000, a)
+plt.xlabel('t /ms')
+plt.ylabel('Normierte Amplitude')
 
 plt.savefig("imgs/beat.png")
